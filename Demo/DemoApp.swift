b9e43f2489e7d21e50639c68b46795e0d3a6d011
Mohammed Abbas <something@yahoo.com>
2021-10-11 11:52:37 -0400

    My first attempt to committing these files

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Field Employee on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
